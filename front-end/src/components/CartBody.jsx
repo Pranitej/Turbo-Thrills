3f996c51d19376f95b39623787504184da4f3903
Vangala Pranitej <137621721+Pranitej@users.noreply.github.com>
2024-02-23 16:29:01 +0530

    change

--- front-end/src/components/CartBody.jsx
+++ front-end/src/components/CartBody.jsx
@@ -65,7 +65,7 @@ export default function CartBody() {
       <div className="row mb-3 text-light">
       {
         data1 && data1.length === 0 
-        ? <h2 className="mt-5 mb-5 text-center">Cart is Empty</h2>
+        ? <h2 className="mt-5 mb-5">Cart is Empty</h2>
         : <h2>Cars in Cart</h2>
       }
       </div>
